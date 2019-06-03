2e8c565b07b6a22fe95194623bc2480b743e6cc7
Thomas Roger <troger@nuxeo.com>
2019-06-03 17:06:23 +0200

    NXP-27472: bootstrap nuxeo-management-rest-api plugin

---
+++ nuxeo-management-rest-api/src/main/java/org/nuxeo/rest/management/ManagementRoot.java
@@ -0,0 +1,39 @@
+/*
+ * (C) Copyright 2019 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Thomas Roger
+ */
+
+package org.nuxeo.rest.management;
+
+import org.nuxeo.ecm.webengine.model.WebObject;
+import org.nuxeo.ecm.webengine.model.impl.ModuleRoot;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+/**
+ * @since 11.1
+ */
+@Path("/")
+@WebObject(type = "management")
+public class ManagementRoot extends ModuleRoot {
+
+    @Path("{path}")
+    public Object route(@PathParam("path") String path) {
+        return newObject(path);
+    }
+}
---
+++ nuxeo-management-rest-api/src/main/java/org/nuxeo/rest/management/distribution/DistributionObject.java
@@ -0,0 +1,48 @@
+/*
+ * (C) Copyright 2019 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Thomas Roger
+ */
+
+package org.nuxeo.rest.management.distribution;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.nuxeo.ecm.webengine.model.WebObject;
+import org.nuxeo.ecm.webengine.model.impl.DefaultObject;
+
+/**
+ * @since 11.1
+ */
+@WebObject(type = "distribution")
+public class DistributionObject extends DefaultObject {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Object doGet() {
+        Map<String, Serializable> map = new HashMap<>();
+        map.put("foo", "bar");
+        return Response.ok(map).build();
+    }
+
+}
