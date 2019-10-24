e7f7ab6e1aaffbe9b2209e337d3ae2c3ee1c05c0
nalkotob <nalkotob@nuxeo.com>
2019-10-24 16:28:53 +0200

    NXP-28105: Refactor BAF dependant tests

--- nuxeo-management-rest-api/src/test/java/org/nuxeo/rest/management/TestRenditionsObject.java
+++ nuxeo-management-rest-api/src/test/java/org/nuxeo/rest/management/TestRenditionsObject.java
@@ -20,28 +20,21 @@
 package org.nuxeo.rest.management;
 
 import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
-import static javax.servlet.http.HttpServletResponse.SC_OK;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-import static org.nuxeo.ecm.core.bulk.io.BulkConstants.STATUS_COMMAND_ID;
-import static org.nuxeo.ecm.core.bulk.io.BulkConstants.STATUS_ENTITY_TYPE;
 import static org.nuxeo.ecm.core.bulk.io.BulkConstants.STATUS_ERROR_COUNT;
 import static org.nuxeo.ecm.core.bulk.io.BulkConstants.STATUS_ERROR_MESSAGE;
 import static org.nuxeo.ecm.core.bulk.io.BulkConstants.STATUS_HAS_ERROR;
 import static org.nuxeo.ecm.core.bulk.io.BulkConstants.STATUS_PROCESSED;
 import static org.nuxeo.ecm.core.bulk.io.BulkConstants.STATUS_TOTAL;
-import static org.nuxeo.ecm.core.io.registry.MarshallingConstants.ENTITY_FIELD_NAME;
 import static org.nuxeo.ecm.platform.picture.api.adapters.AbstractPictureAdapter.VIEWS_PROPERTY;
 
 import java.io.IOException;
 import java.io.Serializable;
 import java.nio.charset.StandardCharsets;
-import java.time.Instant;
-import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -49,7 +42,6 @@ import javax.inject.Inject;
 import javax.ws.rs.core.MultivaluedMap;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.nuxeo.common.utils.FileUtils;
 import org.nuxeo.ecm.automation.test.AutomationFeature;
 import org.nuxeo.ecm.core.api.Blob;
@@ -58,12 +50,9 @@ import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.DocumentRef;
 import org.nuxeo.ecm.core.api.thumbnail.ThumbnailService;
-import org.nuxeo.ecm.core.bulk.message.BulkStatus.State;
 import org.nuxeo.jaxrs.test.CloseableClientResponse;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
-import org.nuxeo.runtime.test.runner.FeaturesRunner;
-import org.nuxeo.runtime.test.runner.TransactionalFeature;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
@@ -71,7 +60,6 @@ import com.sun.jersey.core.util.MultivaluedMapImpl;
 /**
  * @since 11.1
  */
-@RunWith(FeaturesRunner.class)
 @Features(AutomationFeature.class)
 @Deploy("org.nuxeo.ecm.core.management")
 @Deploy("org.nuxeo.ecm.platform.tag")
@@ -82,7 +70,7 @@ import com.sun.jersey.core.util.MultivaluedMapImpl;
 @Deploy("org.nuxeo.ecm.platform.types.api")
 @Deploy("org.nuxeo.ecm.platform.types.core")
 @Deploy("org.nuxeo.ecm.platform.thumbnail")
-public class TestRenditionsObject extends ManagementBaseTest {
+public class TestRenditionsObject extends ManagementBAFTest {
 
     public static final String STATE = "state";
 
@@ -102,9 +90,6 @@ public class TestRenditionsObject extends ManagementBaseTest {
 
     @Inject
     protected CoreSession session;
-
-    @Inject
-    protected TransactionalFeature txFeature;
 
     @Inject
     protected ThumbnailService thumbnailService;
@@ -238,53 +223,6 @@ public class TestRenditionsObject extends ManagementBaseTest {
         assertEquals(1, result.get(STATUS_ERROR_COUNT).asInt());
         assertEquals(0, result.get(STATUS_TOTAL).asInt());
         assertEquals(INVALID_QUERY, result.get(STATUS_ERROR_MESSAGE).asText());
-    }
-
-    protected JsonNode validateBulkStatus(CloseableClientResponse response) throws IOException {
-        assertEquals(SC_OK, response.getStatus());
-        JsonNode result = mapper.readTree(response.getEntityInputStream());
-        assertEquals(STATUS_ENTITY_TYPE, result.get(ENTITY_FIELD_NAME).asText());
-        assertTrue(result.has(STATE));
-        assertTrue(result.has("processed"));
-        assertTrue(result.has("error"));
-        assertTrue(result.has("errorCount"));
-        assertTrue(result.has("total"));
-        assertTrue(result.has("action"));
-        assertTrue(result.has("username"));
-        assertTrue(result.has("submitted"));
-        assertTrue(result.has("scrollStart"));
-        assertTrue(result.has("scrollEnd"));
-        assertTrue(result.has("processingStart"));
-        assertTrue(result.has("processingEnd"));
-        assertTrue(result.has("completed"));
-        assertTrue(result.has("processingMillis"));
-        return result;
-    }
-
-    protected JsonNode runBulkAction(String extraPath, MultivaluedMap<String, String> formData) throws IOException {
-        String postPath = PATH + extraPath;
-        String commandId;
-        try (CloseableClientResponse response = httpClientRule.post(postPath, formData)) {
-            JsonNode result = validateBulkStatus(response);
-            assertEquals(State.SCHEDULED.name(), result.get(STATE).asText());
-            commandId = result.get(STATUS_COMMAND_ID).asText();
-
-        }
-        // waiting for the asynchronous BAF task to finish
-        txFeature.nextTransaction();
-
-        // checking the bulk action is completed
-        try (CloseableClientResponse response = httpClientRule.get(postPath + "/" + commandId)) {
-            JsonNode result = validateBulkStatus(response);
-            assertEquals(State.COMPLETED.name(), result.get(STATE).asText());
-            try {
-                Instant.parse(result.get("completed").asText());
-            } catch (DateTimeParseException e) {
-                fail("parsing should not fail since we're expecting a well-formed date");
-            }
-            assertNotNull(result.get("processingMillis"));
-            return result;
-        }
     }
 
     @SuppressWarnings("unchecked")
